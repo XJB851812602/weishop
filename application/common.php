dad70317e1ddf3011287d87445aeb212788cfca3
XJB851812602 <851812602@qq.com>
2018-07-30 14:39:32 +0800

    测试

--- application/common.php
+++ application/common.php
@@ -1,12 +1,28 @@
 <?php
-// +----------------------------------------------------------------------
-// | ThinkPHP [ WE CAN DO IT JUST THINK ]
-// +----------------------------------------------------------------------
-// | Copyright (c) 2006-2016 http://thinkphp.cn All rights reserved.
-// +----------------------------------------------------------------------
-// | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
-// +----------------------------------------------------------------------
-// | Author: 流年 <liu21st@gmail.com>
-// +----------------------------------------------------------------------
 
-// 应用公共文件
+/**
+ * @param string $url post请求地址
+ * @param array $params
+ * @return mixed
+ */
+function curl_post($url, array $params = array())
+{
+    $data_string = json_encode($params);
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_HEADER, 0);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 10);
+    curl_setopt($ch, CURLOPT_POST, 1);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);
+    curl_setopt(
+        $ch, CURLOPT_HTTPHEADER,
+        array(
+            'Content-Type: application/json'
+        )
+    );
+    $data = curl_exec($ch);
+    curl_close($ch);
+    return ($data);
+}
